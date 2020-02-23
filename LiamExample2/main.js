8837f1215d4c213b8db1c7a749f1d7cd64cde0b3
LiamMelin <60385913+LiamMelin@users.noreply.github.com>
2020-02-23 17:35:58 +0100

    Update main.js
    Commenting the code in example two

--- LiamExample2/main.js
+++ LiamExample2/main.js
@@ -3,33 +3,36 @@ var two = new Two({
   autostart: true
 }).appendTo(document.body);
 
+//Creating shapes 
 var star = two.makeStar(300, 350, 65, 100);
 var rect = two.makeRectangle(450, 350, 100, 100);
 var poly = two.makePolygon(295, 200, 60, 8);
 var ellip = two.makeEllipse(450, 200, 70, 40);
 
+//Setting the colour 
 star.fill = "orange";
 star.stroke = "orangered";
 rect.fill = "blue";
+rect.noStroke();
 poly.stroke = "turquoise";
 ellip.fill = "lightgreen";
 ellip.stroke = "green";
 
-rect.noStroke();
 
+//Creating a container object to hold the shapes
 var group = two.makeGroup(star, rect, poly, ellip);
 group.translation.set(1150, 200);
 group.linewidth = 10;
 
-
+//Spinning the shapes in a circle as they scale up in size 
   two.bind("update", function(frameCount) {
-    if (group.scale > 0.9999) {
+    if (group.scale > 0.999) {
       group.scale = group.rotation = 0;
     }
     var t = (1 - group.scale) * 0.075;
     group.scale += t;
     group.rotation += t * 0.75 * Math.PI;
-    
+    //Rotating each shape 
     ellip.rotation += 0.05;
     star.rotation += 0.05;
     rect.rotation += 0.05;
