cab2b145363f611722f3033134517eeea268fc9d
solaiman2020 <60646410+solaiman2020@users.noreply.github.com>
2020-02-28 16:08:51 +0100

    Solaiman readme 1st example

--- Solaiman example-2/main.js
+++ Solaiman example-2/main.js
@@ -1,20 +1,21 @@
-var elem = document.getElementById('draw-shapes'); //Add the id of your div inside the quotes//
-var params = { fullscreen: true }; //To set whole browser window as the canvas; otherwise, specify dimensions//
-var two = new Two(params).appendTo(elem); //An instance of Two, which is necessary to append the TwoJS to the DOM//
+var elem = document.getElementById('draw-shapes'); // link to the DOM
+var params = { fullscreen: true }; //To set whole browser window as the canvas
+var two = new Two(params).appendTo(elem); // link to the element contained in the DOM
 
+// create different shapes with two.js  built-in methods
 var poly = two.makePolygon(150, 100, 80, 6);
 var rou = two.makeRoundedRectangle(400, 100, 200, 120, 6);
 var tri = two.makePolygon(150, 300, 100, 3);
 var squ = two.makeRectangle(400, 270, 150, 150);
 
+// filling the shapes with different colour
 poly.fill = 'gray';
 rou.fill = 'pink',
 tri.fill = 'orange';
 squ.fill = 'green';
 
-
+// make a group with the variables and style them 
 group = two.makeGroup(poly, rou, tri, squ);
-
 group.linewidth = 4;
 group.stroke = 'black';
 group.opacity = 0.5;
---
+++ solaiman-example-1/README.md
@@ -0,0 +1,5 @@
+In this example I followed the Basic Uses from two.js library.
+By using this library i created 5 orange circles in random positions and later an event listener to draw white circles over those orange circles depending on mouse event where the mouse has being clicked.
+This prototype can be used to provide an interactive feedback to the user that it responds when the user is interacting with the mouse. 
+For this example I took help from the two.js library Basic Usage https://two.js.org/#two-rounded-rectangle
+(This is Solaiman's example 1)
